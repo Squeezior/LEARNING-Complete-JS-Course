eb2fabd1c5951cbdb2926bb47303a314009de807
Squeezior <mrsqueezeme@gmail.com>
2022-03-07 19:53:03 +0100

    Merge branch 'main' of https://github.com/Squeezior/Learning-Complete-JS-Course

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -278,3 +278,87 @@ const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(200));
 console.log(addVAT2(40));
 */
+
+///////////////////////////////////////////////
+// Coding Challenge #1
+
+/*
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // console.log(this.answers);
+    // const [indexArr, valueArr] = this.answers;
+    const questionPrompt = Number(
+      prompt(`What is your favourite programming language?
+      0: JavaScript
+      1: Python
+      2: Rust
+      3: C++
+      (Write option number)`)
+    );
+    console.log(questionPrompt);
+    if (questionPrompt >= 0 && questionPrompt < 4) {
+      for (let i = 0; i < this.answers.length; i++) {
+        if (questionPrompt === i) this.answers[i] += 1;
+        console.log(this.answers);
+      }
+    } else {
+      console.log(`Please answer with a number in range 0-3`);
+    }
+  },
+  displayResults(type) {
+    console.log(this.answers);
+  },
+};
+
+// poll.registerNewAnswer();
+poll.registerNewAnswer(displayResults);
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    // console.log(this.answers);
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+// poll.registerNewAnswer();
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
