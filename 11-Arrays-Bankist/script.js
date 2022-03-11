bbc76f0ab084d4dc4ab88798f8cb0640c084ad34
Squeezior <mrsqueezeme@gmail.com>
2022-03-11 16:57:10 +0100

    Array tools/methods

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -74,3 +74,42 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+// does NOT mutate the original array
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice()); //shallow copy
+console.log([...arr]);
+
+// SPLICE
+// mutates the original array
+// console.log(arr.splice(2));
+arr.splice(-1); // deleting last element
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+// mutates the original array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+// does NOT mutate the original array
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+// does NOT mutate the original array
+console.log(letters.join('-'));
+*/
