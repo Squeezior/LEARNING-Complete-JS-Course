7367eeea460e964a777b11fe4125ff029e2597ae
Squeezior <mrsqueezeme@gmail.com>
2022-03-11 17:04:03 +0100

    AT method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -113,3 +113,19 @@ console.log([...arr, ...arr2]);
 // does NOT mutate the original array
 console.log(letters.join('-'));
 */
+
+///////////////////////////////////////////////
+// The new AT method (ES2022)
+/*
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting the last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('matt'.at(0));
+console.log('matt'.at(-1));
+*/
