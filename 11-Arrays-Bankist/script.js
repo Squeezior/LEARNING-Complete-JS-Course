fbdd5cbded7bd45cb856fb65bcd1ee5873f97bc8
Squeezior <mrsqueezeme@gmail.com>
2022-03-14 20:58:18 +0100

    Find Method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -466,3 +466,26 @@ const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
 */
+
+//////////////////////////////////////////////////
+// The Find Method
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// does NOT return the new array, only first element for which the operation is true
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+
+// let accountFor = {};
+// for (const acc of accounts) {
+//   if (acc.owner === 'Jessica Davis') {
+//     accountFor = acc;
+//   }
+// }
+// console.log(accountFor);
+*/
