6b1fc358beff496e9bdebb76cd37e1c9a4ca48e0
Squeezior <mrsqueezeme@gmail.com>
2022-03-15 13:24:08 +0100

    Some and Every Methods

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -187,6 +187,21 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -574,4 +589,34 @@ console.log(account);
 //   }
 // }
 // console.log(accountFor);
+*/
+
+/////////////////////////////////////////////////
+// Some and Every
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// SOME
+
+// EQUALITY
+console.log(movements);
+console.log(movements.includes(-130));
+
+// CONDITION
+console.log(movements.some(mov => mov === -130));
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+// if there is any value for which this condition is true, then the some method will return true
+
+// EVERY
+
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
 */
