142c2d1a9b695e38015efb24e6e9ec683c25cbb9
Squeezior <mrsqueezeme@gmail.com>
2022-03-15 14:28:18 +0100

    Creating and Filling Arrays

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -662,7 +662,7 @@ console.log(overallBalance);
 
 /////////////////////////////////////////////////
 // Sort
-
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // Strings
@@ -692,3 +692,45 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+
+/////////////////////////////////////////////////
+// More ways of creating and filling arrays
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+x.fill(1, 3, 5);
+x.fill(1); // mutates the array
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1); // we use the .form on the constructor
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+const diceRolls = Array.from(
+  { length: 100 },
+  () => Math.trunc(Math.random() * 6) + 1
+);
+
+console.log(diceRolls);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'), // creates an array from array-like object
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')]; // creates an array from array-like object
+});
