768300cf00a13fc687c480e1a2c2392fb8ce9aa7
Squeezior <mrsqueezeme@gmail.com>
2022-03-29 13:24:35 +0200

    Building a Tabbed Component

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -80,6 +80,32 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// tabs.forEach(t => t.addEventListener('click', () => console.log('TAB'))); // BAD practice
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 // Selecting, Creating and Deleting Elements
@@ -256,7 +282,7 @@ document.querySelector('.nav').addEventListener(
 
 ////////////////////////////////////////////////////
 // DOM Traversing
-
+/*
 const h1 = document.querySelector('h1');
 
 // Going downwards: child
@@ -287,3 +313,4 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5';
 });
+*/
