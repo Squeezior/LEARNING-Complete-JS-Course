cab614c0c889b59858c5423b956ba017f1c33748
Squeezior <mrsqueezeme@gmail.com>
2022-03-29 13:49:48 +0200

    Passing Arguments to Event Handlers

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -6,7 +6,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
-
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 ///////////////////////////////////////
 // Modal window
 
@@ -81,9 +84,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 // Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 // tabs.forEach(t => t.addEventListener('click', () => console.log('TAB'))); // BAD practice
 
@@ -105,6 +105,55 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener(
+  'mouseover',
+  handleHover.bind(0.5)
+  // function (e) {
+  //   handleHover(e, 0.5);
+  // }
+  // if (e.target.classList.contains('nav__link')) {
+  //   const link = e.target;
+  //   const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+  //   const logo = link.closest('.nav').querySelector('img');
+  //   siblings.forEach(el => {
+  //     if (el !== link) el.style.opacity = 0.5;
+  //   });
+  //   logo.style.opacity = 0.5;
+  // }
+);
+
+nav.addEventListener(
+  'mouseout',
+  handleHover.bind(1)
+  // function (e) {
+  //   handleHover(e, 1);
+  // }
+  // if (e.target.classList.contains('nav__link')) {
+  //   const link = e.target;
+  //   const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+  //   const logo = link.closest('.nav').querySelector('img');
+  //   siblings.forEach(el => {
+  //     if (el !== link) el.style.opacity = 1;
+  //   });
+  //   logo.style.opacity = 1;
+  // }
+);
 
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
