2f3b3fee8f8c89407c16fe91fc1e08dbc19639f5
Squeezior <mrsqueezeme@gmail.com>
2022-04-01 12:24:04 +0200

    Constructor Functions and the new Operator

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// Constructor Function
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this, never create a method inside of a constructor function
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New empty object {} is created
+// 2. Function is called, this keyword = new enmpty object, this = {}
+// 3. {} linked to prototype
+// 4. Function automatically returns that object from the beggining {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
