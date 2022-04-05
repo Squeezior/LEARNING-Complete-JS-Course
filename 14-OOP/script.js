ceb0cc7330d62148d606846192a0d42963518dd7
Squeezior <mrsqueezeme@gmail.com>
2022-04-05 15:00:36 +0200

    Inheritance Between "Classes": Object.create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -381,6 +381,10 @@ tesla.brake();
 console.log(tesla);
 */
 
+////////////////////////////////////////////////////
+// Inheritance Between "Classes": ES6 Classes
+
+/*
 class PersonCl {
   constructor(fullName, birthYear) {
     this.fullName = fullName;
@@ -444,3 +448,35 @@ const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 
 martha.introduce();
 martha.calcAge();
+*/
+
+////////////////////////////////////////////////////
+// Inheritance Between "Classes": Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
