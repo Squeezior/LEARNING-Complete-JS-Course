5adce2d07db33eeeb52cf0181bfe9b8efaf456ee
Squeezior <mrsqueezeme@gmail.com>
2022-04-05 18:24:08 +0200

    Encapsulation: Private Class Fields and Methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -482,33 +482,43 @@ jay.introduce();
 jay.calcAge();
 */
 
+// 1.) Public fields
+// 2.) Private fields
+// 3.) Public methods
+// 4.) Private methods
+// (there is also the static version of above)
+
 class Account {
+  // 1.) Public fields (instances)
+  locale = navigator.language;
+
+  // 2.) Private fields (instances)
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
     // Protected property
-    this._pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  // 3.) Public methods
   // Public Interface (API)
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
-  }
-
-  _approveLoan(val) {
-    return true;
   }
 
   requestLoan(val) {
@@ -516,6 +526,16 @@ class Account {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+  }
+
+  static helper() {
+    console.log(`Helper`);
+  }
+
+  // 4.) Private methods
+  // #approveLoan(val) { will work in the future
+  _approveLoan(val) {
+    return true;
   }
 }
 
@@ -531,4 +551,9 @@ acc1.requestLoan(1000);
 
 console.log(acc1.getMovements());
 console.log(acc1);
-console.log(acc1._pin);
+
+// console.log(acc1.#pin);
+// console.log(acc1.#movements);
+// console.log(acc1.#approveLoan(1000));
+
+Account.helper();
