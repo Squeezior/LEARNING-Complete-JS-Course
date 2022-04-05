279b6279117acb236004620bc35d509eaf27b7b5
Squeezior <mrsqueezeme@gmail.com>
2022-04-05 19:04:03 +0200

    Coding Challenge #4

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -487,7 +487,7 @@ jay.calcAge();
 // 3.) Public methods
 // 4.) Private methods
 // (there is also the static version of above)
-
+/*
 class Account {
   // 1.) Public fields (instances)
   locale = navigator.language;
@@ -563,3 +563,74 @@ Account.helper();
 // Chaining
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const bmw = new CarCl('BMW', 120);
+const mercedes = new CarCl('Mercedes', 95);
+
+// Ford going at 120km/h
+
+const ford = new CarCl('Ford', 120);
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    console.log(`${this.make} is charged to ${this.#charge}%`);
+    return this;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    console.log(
+      `${this.make}'s going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }`
+    );
+    return this;
+  }
+}
+
+// Rivian going at 120 km/h, with a charge of 23%
+
+const rivian = new EVCl('Rivian', 120, 23);
+
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(85)
+  .accelerate()
+  .accelerate()
+  .brake()
+  .accelerate();
+
+console.log(rivian.speedUS);
