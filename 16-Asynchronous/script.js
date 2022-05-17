34731fa87761d987db9be45c59927a658a739fa0
Squeezior <mrsqueezeme@gmail.com>
2022-05-17 13:02:11 +0200

    Welcome to Callback Hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -6,7 +6,7 @@ const body = document.body;
 body.style.backgroundColor = '#292929';
 ///////////////////////////////////////
 // Old school AJAX
-
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -33,6 +33,73 @@ const getCountryData = function (country) {
     countriesContainer.style.opacity = 1;
   });
 };
+*/
+// getCountryData('poland');
+// getCountryData('usa');
 
-getCountryData('poland');
-getCountryData('usa');
+/////////////////////////////////////////////////////////
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} million</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    // console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      // console.log(this.responseText);
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1s passed');
+  setTimeout(() => {
+    console.log('2s passed');
+    setTimeout(() => {
+      console.log('3s passed');
+      setTimeout(() => {
+        console.log('4s passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
