b9eb96b23db7323e99bba96d9126035e62ba7aa1
Squeezior <mrsqueezeme@gmail.com>
2022-07-14 12:20:51 +0200

    Top-level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -20,3 +20,29 @@ add('apples', 4);
 add('oranges', 2);
 
 console.log(cart);
+
+// Top-level await in modules
+
+// blocks the execution of the whole module
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
